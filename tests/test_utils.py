86944cdf5908567cdc798c85e8f22de3cbc57b8d
Rajat Dogra <rajat98dogra@gmail.com>
2026-04-02 17:06:18 +0530

    added testcase format

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -247,8 +247,9 @@ async def test_extract_error_json():
     with pytest.raises(KeyError):
         result["error"]["code"]
 
+
 def test_script_updated_event_requires_interim_script():
-    #test case for UtteranceBotActionScriptUpdated event
+    # test case for UtteranceBotActionScriptUpdated event
     with pytest.raises(AssertionError, match=r".*interim_script.*"):
         event_type = "UtteranceBotActionScriptUpdated"
         e = new_event_dict(
